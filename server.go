021cdc8e19a9a4629daa13ae6134b314f7d00ce9
cheuzard <hichem172019@gmail.com>
2024-08-26 15:46:12 +0100

    pre-serv v1.1

--- server.go
+++ server.go
@@ -20,8 +20,9 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
-			fmt.Printf("error in spliting port %v", err)
+			log.Printf("error in spliting port %v", err)
 		}
+		log.Printf("%v", ip)
 		cheu, _ := net.LookupHost("cheuzard.ddns.net")
 		return ip == cheu[0]
 	},
@@ -37,21 +38,21 @@ var Messages = make(chan string, 70)
 var uList = sync.Map{}
 
 func main() {
-	fmt.Printf("please input the port(press enter to use default :8080):\n")
-	_, err := fmt.Scanf("%v", &port)
-	if err != nil {
+	fmt.Print("Please input the port (press enter to use default: 8080): \n")
+	if _, err := fmt.Scanln(&port); err != nil {
 		port = "8080"
-		fmt.Printf("default port used\n")
+		fmt.Println("Default port used")
 	}
+
 	http.HandleFunc("/", connection)
-	//println("handle func set")
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	go startServer(ctx)
-	select {
-	case <-ctx.Done():
-		stop()
-		fmt.Printf("shutting down")
-	}
+
+	<-ctx.Done()
+	log.Println("Shutting down")
 }
 func startServer(ctx context.Context) {
 	finalPort := fmt.Sprintf(":%v", port)
@@ -60,145 +61,130 @@ func startServer(ctx context.Context) {
 
 	go func() {
 		<-ctx.Done()
-		err := server.Shutdown(context.Background())
-		if err != nil {
-			fmt.Printf("\nerror at shutdown  %v", err)
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Printf("\nerror at shutdown  %v", err)
 			return
 		}
 	}()
-	fmt.Printf("listening on port:%v\n", port)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+
+	log.Printf("listening on port:%v\n", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("server error: %v", err)
 	}
-	fmt.Printf("server dead")
+
+	log.Println("server stopped")
 }
 
 func broadcaster() {
-	fmt.Printf("broadcaster is up")
+	log.Println("broadcaster is up")
 	for {
 		select {
 		case msg := <-Messages:
-			fmt.Printf("sending...\n")
+			log.Println("sending...")
 			index := strings.Index(msg, "]")
 			uList.Range(func(key, value interface{}) bool {
 				if index != -1 && value.(*client).name == msg[1:index] {
 					return true
-				} else {
-					value.(*client).incoming <- msg
-					return true
 				}
+				value.(*client).incoming <- msg
+				return true
 			})
-
 		}
 	}
 }
 
 func connection(w http.ResponseWriter, r *http.Request) {
 	origin := r.Header.Get("Origin")
-	fmt.Printf("connection started%v\n", origin)
+	log.Printf("connection started %v\n", origin)
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Printf("\n\nconnection failed to start: %v\n", err)
+		log.Printf("connection failed to start: %v\n", err)
 		return
 	}
-	println("connection upgraded")
+	log.Println("connection upgraded")
 	client := &client{
 		name:     NameGetter(conn),
 		conn:     conn,
 		incoming: make(chan string, 10),
 	}
-	fmt.Printf("client created : %v\n", client.name)
+	log.Printf("client created: %v\n", client.name)
 	Messages <- fmt.Sprintf("  >%v just joined", client.name)
-	fmt.Printf("  >%v just joined", client.name)
+	log.Printf("  >%v just joined", client.name)
 	go HandleUser(client)
 }
 
 func HandleUser(client *client) {
-	//println("user handler started")
 	uList.Store(client.name, client)
 	ctx, cancel := context.WithCancel(context.Background())
-	defer uList.Delete(client.name)
-	defer cancel()
-	defer func(conn *websocket.Conn) {
-		err := conn.Close()
-		if err != nil {
-			return
-		}
-	}(client.conn)
+	defer func() {
+		uList.Delete(client.name)
+		cancel()
+		client.conn.Close()
+	}()
 	go Receiver(client, ctx, cancel)
 
 	for {
 		select {
 		case msg := <-client.incoming:
-			err := client.conn.WriteMessage(websocket.TextMessage, []byte(msg))
-			if err != nil {
+			if err := client.conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
 				if websocket.IsUnexpectedCloseError(err) {
 					uList.Delete(client.name)
 				}
 				return
 			}
 		case <-ctx.Done():
-			fmt.Printf("terminating user\n")
+			log.Println("terminating user")
 			return
 		}
 	}
 }
 
 func Receiver(client *client, ctx context.Context, cancel context.CancelFunc) {
-	println("receiver started")
+	defer func() {
+		Messages <- fmt.Sprintf("  >%v just left", client.name)
+		log.Printf("  >%v just left", client.name)
+		close(client.incoming)
+		uList.Delete(client.name)
+		cancel()
+		if err := client.conn.Close(); err != nil {
+			return
+		}
+	}()
+
+	log.Println("receiver started")
 	for {
 		_, messageByte, err := client.conn.ReadMessage()
-		fmt.Printf("received :[%v] %v\n", client.name, string(messageByte))
 		if err != nil {
-			if websocket.IsUnexpectedCloseError(err) {
-				Messages <- fmt.Sprintf("  >%v just left", client.name)
-				fmt.Printf("  >%v just left", client.name)
-				close(client.incoming)
-				uList.Delete(client.name)
-				cancel()
-			}
 			return
 		}
 		message := string(messageByte)
 		if len(message) > 0 && message == "/exit" {
-			Messages <- fmt.Sprintf("  >%v just left", client.name)
-			fmt.Printf("  >%v just left", client.name)
-			close(client.incoming)
-			uList.Delete(client.name)
-			cancel()
-			err := client.conn.Close()
-			if err != nil {
-				return
-			}
 			return
-		} else {
-			select {
-			case Messages <- fmt.Sprintf("[%s]: %s", client.name, message):
-			case <-ctx.Done():
-				return
-			}
+		}
+		select {
+		case Messages <- fmt.Sprintf("[%s]: %s", client.name, message):
+		case <-ctx.Done():
+			return
 		}
 	}
 }
 
 func NameGetter(conn *websocket.Conn) string {
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(100 * time.Millisecond)
 	for {
 		exists := false
 		request := "please input your name (between 3 and 10 characters):\n"
-		err := conn.WriteMessage(websocket.TextMessage, []byte(request))
-		if err != nil {
+		if err := conn.WriteMessage(websocket.TextMessage, []byte(request)); err != nil {
 			return ""
 		}
 		_, answer, err := conn.ReadMessage()
 		if err != nil {
-			fmt.Printf("failed getting name")
+			fmt.Println("failed getting name")
 			return ""
 		}
 		name := strings.TrimSpace(string(answer))
-		if len(name) > 3 && len(name) < 11 {
-			uList.Range(func(key, value interface{}) bool {
+		if len(name) >= 3 && len(name) <= 10 {
+			uList.Range(func(key, _ interface{}) bool {
 				if key == name {
 					exists = true
 					return false
@@ -209,14 +195,12 @@ func NameGetter(conn *websocket.Conn) string {
 				return name
 			}
 		} else {
-			err = conn.WriteMessage(websocket.TextMessage, []byte("\n\t!!!name too small or too long!!!\n"))
-			if err != nil {
+			if err := conn.WriteMessage(websocket.TextMessage, []byte("\n\t!!!name too small or too long!!!\n")); err != nil {
 				return ""
 			}
 		}
 		if exists {
-			err = conn.WriteMessage(websocket.TextMessage, []byte("\n\t!!!name already exists!!!\n"))
-			if err != nil {
+			if err := conn.WriteMessage(websocket.TextMessage, []byte("\n\t!!!name already exists!!!\n")); err != nil {
 				return ""
 			}
 		}
